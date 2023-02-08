637082243b2fd86ff1289bdcf62dcc4e16769772
Jia-Wei-Liao <sam23582211@gmail.com>
2023-02-08 16:20:35 +0800

    Upload hw2 dataset

--- HW2/download_dataset.sh
+++ HW2/download_dataset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ ! -f data ]; then
-    gdown https://drive.google.com/uc?id=1FBPQCzUxY4C3tm8lCVXePKper9U1Jd0K -O dataset.zip
+    gdown https://drive.google.com/uc?id=1FLP4PS8i5SZEHY4oRLBybqTqfpBGMoTW -O dataset.zip
     unzip dataset.zip
 fi
